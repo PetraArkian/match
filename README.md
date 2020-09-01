5640a8ca3f5d67bce7acf829d85161d5f38e7b5d
Samantha Lincroft <petraarkian.heg.gov@gmail.com>
2020-09-01 12:11:38 +1000

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# match
-A program to match Big's and Little's at Wellesley College.
+Sample Application with Python and Flask
+========================================
+
+Running Locally
+---------------
+
+```sh
+virtualenv
+. bin/activate
+pip install -r requirements.txt
+python app.py
+```
+
+Deploying on Scalingo
+---------------------
+
+Create an application on https://scalingo.com, then:
+
+```
+git remote add scalingo git@scalingo.com:<name_of_your_app>.git
+git push scalingo master
+```
+
+And that's it!
+
+The application is running at this url: https://sample-python-flask.scalingo.io/
---
+++ app.py
@@ -0,0 +1,47 @@
+import os
+from flask import Flask, flash, request, redirect, url_for
+from flask import render_template
+from werkzeug.utils import secure_filename
+
+
+UPLOAD_FOLDER = 'uploads'
+ALLOWED_EXTENSIONS = set(['txt', 'tsv', 'csv'])
+
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('upload_file',
+                                    filename=filename))
+    return render_template('index.html')
+
+
+@app.route("/view_matches")
+def saveFile():
+    return render_template('matches.html')
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
